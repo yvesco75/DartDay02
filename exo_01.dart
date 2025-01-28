1eac1d8f5b00ed6e3888e8646cbeffa049290208
Yvesco75 <yvesgodonou4@gmail.com>
2025-01-28 11:23:36 +0100

    Ajout du 🔍 Protocole Alpha

---
+++ exo_01.dart
@@ -0,0 +1,26 @@
+
+
+
+// 🔍 Protocole Alpha 
+void main() {
+  var lettre = '''Cher agent X,
+
+
+
+J'ai aperçu le chat noir    au point de rendez-vous 7. L'agent Johnson était 
+
+également présent.    Le code secret 42 a été activé.
+
+
+
+Notre agent de liaison vous contactera à 23h.
+
+
+
+    Cordialement''';
+  
+  print(lettre);
+  print(lettre.length);
+  print(lettre. split(" "));
+}
+
